8526ef6becbe58645481f4617561006b9644cb68
Matheus Valandro <msvalandro@gmail.com>
2019-06-10 16:30:06 -0300

    including forms

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -27,4 +27,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.igChangedSubscription.unsubscribe();
   }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
 }
